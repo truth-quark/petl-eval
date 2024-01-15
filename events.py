33b5d7d2998382ee3fc4d53d3040c57ca6815065
Ben Davies <bens.public@gmail.com>
2024-01-15 22:12:47 +1100

    Split event titles by date/title/tag regex.

--- events.py
+++ events.py
@@ -5,6 +5,11 @@ import os
 import sys
 import petl
 
+# regexes
+TRIP_TITLE_PATTERN = r"^(?P<date>\d{1,2}\/\d{1,2}\/\d{4})" \
+                     r"[ to]*(?P<end_date>\d{1,2}\/\d{1,2}\/\d{4}){0,1} - " \
+                     r"(?P<title>[!&/+,\w\'\"\(\) ]*[a-zA-Z0-9])[ ]?(?P<tags>\[.*\])?$"
+
 
 # TODO: test the following
 # load raw data
@@ -17,4 +22,11 @@ data_path = sys.argv[1]
 assert os.path.exists(data_path)
 
 raw_table = petl.fromtext(data_path)
-print(raw_table)
+
+tokenised_table = petl.capture(raw_table,
+                               "lines",
+                               TRIP_TITLE_PATTERN,
+                               ["date", "end_date", "title", "raw_tags"])
+
+print(tokenised_table)
+
